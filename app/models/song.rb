f7f44699c2396a42378eb42644b5075fc74d4d72
YilKanda <yilkanda@hotmail.com>
2020-12-06 20:10:58 -0600

    Incorporation of Styles
    A Material Design for Bootstrap template was incorporated, to obtain a view similar to a mobile device, like iPod.
    
    The code was also analyzed with the Rubocop tool in order to find something in the code that escapes from the guidelines of the Ruby Style Guide.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Song < ApplicationRecord
   has_one_attached :audio
   has_one_attached :image
--- test/system/songs_test.rb
+++ test/system/songs_test.rb
@@ -1,49 +1,51 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class SongsTest < ApplicationSystemTestCase
   setup do
     @song = songs(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit songs_url
-    assert_selector "h1", text: "Songs"
+    assert_selector 'h1', text: 'Songs'
   end
 
-  test "creating a Song" do
+  test 'creating a Song' do
     visit songs_url
-    click_on "New Song"
+    click_on 'New Song'
 
-    fill_in "Album", with: @song.album
-    fill_in "Artist", with: @song.artist
-    fill_in "Genre", with: @song.genre
-    fill_in "Name", with: @song.name
-    click_on "Create Song"
+    fill_in 'Album', with: @song.album
+    fill_in 'Artist', with: @song.artist
+    fill_in 'Genre', with: @song.genre
+    fill_in 'Name', with: @song.name
+    click_on 'Create Song'
 
-    assert_text "Song was successfully created"
-    click_on "Back"
+    assert_text 'Song was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a Song" do
+  test 'updating a Song' do
     visit songs_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    fill_in "Album", with: @song.album
-    fill_in "Artist", with: @song.artist
-    fill_in "Genre", with: @song.genre
-    fill_in "Name", with: @song.name
-    click_on "Update Song"
+    fill_in 'Album', with: @song.album
+    fill_in 'Artist', with: @song.artist
+    fill_in 'Genre', with: @song.genre
+    fill_in 'Name', with: @song.name
+    click_on 'Update Song'
 
-    assert_text "Song was successfully updated"
-    click_on "Back"
+    assert_text 'Song was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a Song" do
+  test 'destroying a Song' do
     visit songs_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "Song was successfully destroyed"
+    assert_text 'Song was successfully destroyed'
   end
 end
